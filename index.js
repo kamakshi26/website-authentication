a30d68da3e6e53c3b82c788f1d34006ba608821a
kamakshi jayaraman <53111556+kamakshi26@users.noreply.github.com>
2020-05-06 23:43:13 +0530

    level 6 security
    sign in using third part websites
    using passport-google-oAuth  package

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const md5 = require("md5");
 const session = require('express-session');
 const passport=require('passport');
 const passportlocalmongoose=require('passport-local-mongoose');
+const findOrCreate = require('mongoose-findorcreate');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -36,17 +38,52 @@ mongoose.set('useCreateIndex', true);//to avoid deprecated warning
 // create user schema
 const userSchema = new mongoose.Schema({
   email: String,//username
-  password: String
+  password: String,
+  googleId: String,
+  secret : String
 });
 
 userSchema.plugin(passportlocalmongoose);
+userSchema.plugin(findOrCreate);
 // create usermodel
 const userModel = mongoose.model("user", userSchema);
 
 passport.use(userModel.createStrategy());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
 
-passport.serializeUser(userModel.serializeUser());
-passport.deserializeUser(userModel.deserializeUser());
+passport.deserializeUser(function(id, done) {
+  userModel.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secret",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo" //for google+ deprecation..getting user info
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    userModel.findOrCreate({ googleId: profile.id }, function (err, user) { //save the data that the user do in our website in the form of cookie
+      return cb(err, user);
+    });
+  }
+));
+
+
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+  app.get('/auth/google/secret',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secret');
+  });
 
 app.get('/', function(req, res) {
   res.render("home");
@@ -62,12 +99,17 @@ app.get('/register', function(req, res) {
   res.render("register");
 });
 
-app.get('/secrets',function(req,res){
-  if(req.isAuthenticated()){
-    res.render('secrets');
+app.get('/secret',function(req,res){
+userModel.find({secret : {$ne : null}}, function(err,foundusers){
+  if(err){
+    console.log(err);
   }else{
-    res.redirect('/login');
+    if(foundusers){
+      console.log(foundusers);
+      res.render('secret',{userswithsecrets: foundusers});
+    }
   }
+});
 });
 
 app.post('/register', function(req, res) {
@@ -77,13 +119,35 @@ userModel.register({username : req.body.username}, req.body.password, function(e
     res.send('<h1>seems like you are already registered! try a new user name or else go to login page! thank you:)</h1>');
   }else{
     passport.authenticate("local")(req,res,function(){
-      res.redirect('/secrets');
+      res.redirect('/secret');
     })
   }
 });
 
 });
 
+app.get('/submit',function(req,res){
+  if(req.isAuthenticated()){
+    res.render('submit');
+  }else{
+    res.redirect('/login');
+  }
+});
+app.post('/submit',function(req,res){
+  const submittedsecret=req.body.secret;
+userModel.findById(req.user.id,function(err,found){
+  if(err){
+    console.log(err);
+  }else{
+    if(found){
+      found.secret=submittedsecret;
+      found.save(function(err){
+        res.redirect('/secret');
+      });
+    }
+  }
+});
+});
 app.get('/logout',function(req,res){
   req.logout();
   res.redirect('/');
@@ -102,12 +166,12 @@ req.login(user,function(err){
   }else{
     passport.authenticate("local")(req,res,function(){
 
-          res.redirect('/secrets');
+          res.redirect('/secret');
         })
   }
 });
 });
 
 app.listen(3000, function() {
-  console.log("iam ready");
+  console.log("iam ready princess! good to go love!");
 });
